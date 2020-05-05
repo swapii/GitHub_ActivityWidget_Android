f5ffc3a6f7a72fba34a3eb6073b64febdafd5a05
Pavel Savinov <swapii@gmail.com>
2020-05-05 18:13:41 +0300

    Extract getPageSource to operator fun class

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,7 @@
 object Dependencies {
 
     const val rxJava = "io.reactivex.rxjava3:rxjava:${Versions.rxJava}"
+    const val rxJavaBridge = "com.github.akarnokd:rxjava3-bridge:3.0.0"
     const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
 
     object Slf4j {
@@ -11,6 +12,13 @@ object Dependencies {
     object Dagger {
         const val android = "com.google.dagger:dagger-android:${Versions.dagger}"
         const val compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    }
+
+    object Kotlin {
+        object Coroutines {
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5"
+            const val rx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.3.5"
+        }
     }
 
 }
--- github/src/main/java/com/github/GitHubClient.kt
+++ github/src/main/java/com/github/GitHubClient.kt
@@ -1,10 +1,10 @@
 package com.github
 
+import hu.akarnokd.rxjava3.bridge.RxJavaBridge
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.rx2.rxSingle
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.lang.RuntimeException
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -12,39 +12,37 @@ import java.util.regex.Pattern
 
 class GitHubClient {
 
-	fun getUserActivity(username: String): Observable<OneDayActivityFromServer> =
-		getPageSource(URL("https://github.com/$username"))
-			.flatMapObservable { pageSource -> parseDayActivity(pageSource) }
-			.switchIfEmpty(Observable.error(PageParseException("Can't find any activity on page")))
+    fun getUserActivity(username: String): Observable<OneDayActivityFromServer> =
+        getPageSource(URL("https://github.com/$username"))
+            .flatMapObservable { pageSource -> parseDayActivity(pageSource) }
+            .switchIfEmpty(Observable.error(PageParseException("Can't find any activity on page")))
 
-	private fun getPageSource(url: URL): Single<String> =
-		Single.fromCallable {
-			val client = OkHttpClient()
-			val request = Request.Builder()
-				.url(url)
-				.build()
-			val call = client.newCall(request)
-			call.execute().use { response ->
-				response.body!!.string()
-			}
-		}
+    private fun getPageSource(url: URL): Single<String> =
+        RxJavaBridge.toV3Single(
+            rxSingle {
+                GetWebPageSource(OkHttpClient()).invoke(url)
+            }
+        )
 
-	private fun parseDayActivity(pageSource: String): Observable<OneDayActivityFromServer> =
-		Observable.create { emitter ->
-			val matcher = CELL_PATTERN.matcher(pageSource)
-			while (matcher.find()) {
-				val count = Integer.parseInt(matcher.group(1))
-				val date = DATE_FORMAT.parse(matcher.group(2))
-				emitter.onNext(OneDayActivityFromServer(date, count))
-			}
-			emitter.onComplete()
-		}
+    private fun parseDayActivity(pageSource: String): Observable<OneDayActivityFromServer> =
+        Observable.create { emitter ->
+            val matcher = CELL_PATTERN.matcher(pageSource)
+            while (matcher.find()) {
+                val count = Integer.parseInt(matcher.group(1))
+                val date = DATE_FORMAT.parse(matcher.group(2))
+                emitter.onNext(OneDayActivityFromServer(date, count))
+            }
+            emitter.onComplete()
+        }
 
-	inner class PageParseException(s: String) : RuntimeException(s)
+    inner class PageParseException(s: String) : RuntimeException(s)
 
-	companion object {
-		private val CELL_PATTERN = Pattern.compile("<rect class=\"day\" .+? data-count=\"(\\d+?)\" data-date=\"(\\d{4}-\\d{2}-\\d{2})\"/>", Pattern.DOTALL)
-		private val DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
-	}
+    companion object {
+        private val CELL_PATTERN = Pattern.compile(
+            "<rect class=\"day\" .+? data-count=\"(\\d+?)\" data-date=\"(\\d{4}-\\d{2}-\\d{2})\"/>",
+            Pattern.DOTALL
+        )
+        private val DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
+    }
 
 }
