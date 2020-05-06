0616c36348b157f99b12f35e43bf99f1ac9a12a2
Pavel Savinov <swapii@gmail.com>
2020-05-06 12:00:53 +0300

    Unified Single for getting day activity

--- github/src/main/java/com/github/GitHubClient.kt
+++ github/src/main/java/com/github/GitHubClient.kt
@@ -9,14 +9,11 @@ import java.net.URL
 class GitHubClient {
 
     fun getUserActivity(username: String): Single<List<OneDayActivityFromServer>> =
-        getPageSource(URL("https://github.com/$username"))
-            .flatMap { pageSource -> parseDayActivity(pageSource) }
-
-    private fun getPageSource(url: URL): Single<String> =
-        rxSingle { GetWebPageSource(OkHttpClient())(url) }.toV3()
-
-    private fun parseDayActivity(pageSource: String): Single<List<OneDayActivityFromServer>> =
-        rxSingle { ParseDayActivity()(pageSource) }.toV3()
+            rxSingle {
+                val url = URL("https://github.com/$username")
+                val pageSource = GetWebPageSource(OkHttpClient())(url)
+                ParseDayActivity()(pageSource)
+            }.toV3()
 
     private fun <T> io.reactivex.Single<T>.toV3(): Single<T> = RxJavaBridge.toV3Single(this)
 
