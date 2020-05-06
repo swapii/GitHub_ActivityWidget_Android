50358007d234d9f0bf3a4927ad400b2660fb6ca4
Pavel Savinov <swapii@gmail.com>
2020-05-06 13:53:31 +0300

    Remove AA from MainActivity

--- app/src/main/java/github/activity/ui/MainActivity.java
+++ app/src/main/java/github/activity/ui/MainActivity.java
@@ -3,10 +3,11 @@ package github.activity.ui;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.androidannotations.annotations.EActivity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +15,15 @@ import github.activity.R;
 import github.activity.UpdateService_;
 import github.activity.ui.widget.ActivityWidgetProvider;
 
-@EActivity(R.layout.main)
 public class MainActivity extends AppCompatActivity {
 
 	private static final Logger L = LoggerFactory.getLogger(MainActivity.class);
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
 	@Override
 	protected void onResume() {
