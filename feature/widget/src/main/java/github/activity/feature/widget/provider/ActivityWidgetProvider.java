6366f89d7ef0070073e3e5f79bb3679fa1b60baf
Pavel Savinov <swapii@gmail.com>
2020-10-17 20:53:33 +0300

    Format ActivityWidgetProvider.java

--- feature/widget/src/main/java/github/activity/feature/widget/provider/ActivityWidgetProvider.java
+++ feature/widget/src/main/java/github/activity/feature/widget/provider/ActivityWidgetProvider.java
@@ -28,134 +28,134 @@ import github.activity.feature.widget.presentation.preferences.PreferencesActivi
 
 public class ActivityWidgetProvider extends AppWidgetProvider {
 
-	private static final Logger L = LoggerFactory.getLogger(ActivityWidgetProvider.class);
+    private static final Logger L = LoggerFactory.getLogger(ActivityWidgetProvider.class);
 
-	@Inject
+    @Inject
     GetUserActivity getUserActivity;
 
-	@Override
-	public void onUpdate(Context context, AppWidgetManager widgetManager, int[] widgetIds) {
-		L.trace("On update");
-		inject(context);
-		for (int widgetId : widgetIds) {
-			updateWidget(context, widgetId);
-		}
-	}
-
-	private void inject(Context context) {
-	    if (getUserActivity != null) {
-	        // Already injected
-	        return;
+    @Override
+    public void onUpdate(Context context, AppWidgetManager widgetManager, int[] widgetIds) {
+        L.trace("On update");
+        inject(context);
+        for (int widgetId : widgetIds) {
+            updateWidget(context, widgetId);
         }
-		((WidgetComponent.Provider) context.getApplicationContext()).getWidgetComponent().inject(this);
-	}
+    }
 
-	@Override
-	public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
-		L.trace("Widget {} options changed", appWidgetId);
-		inject(context);
-		updateWidget(context, appWidgetId);
-	}
+    private void inject(Context context) {
+        if (getUserActivity != null) {
+            // Already injected
+            return;
+        }
+        ((WidgetComponent.Provider) context.getApplicationContext()).getWidgetComponent().inject(this);
+    }
 
-	public void updateWidget(Context context, int widgetId) {
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        L.trace("Widget {} options changed", appWidgetId);
+        inject(context);
+        updateWidget(context, appWidgetId);
+    }
 
-		AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
+    public void updateWidget(Context context, int widgetId) {
 
-		ActivityWidget widget = new ActivityWidget(context, widgetId);
+        AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
 
-		String username = widget.getUsername();
+        ActivityWidget widget = new ActivityWidget(context, widgetId);
+
+        String username = widget.getUsername();
 
         List<DailyUserActivity> userActivity = new ArrayList<>(getUserActivity.invoke(username));
 
-		if (userActivity.size() < 365) {
-			return;
-		}
+        if (userActivity.size() < 365) {
+            return;
+        }
 
-		fillUserActivityToFullWeek(userActivity);
+        fillUserActivityToFullWeek(userActivity);
 
-		Intent preferencesActivityIntent = PreferencesActivity.createIntent(context, widgetId);
+        Intent preferencesActivityIntent = PreferencesActivity.createIntent(context, widgetId);
 
-		PendingIntent intent = PendingIntent.getActivity(
-				context, widgetId,
-				preferencesActivityIntent,
-				PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent intent = PendingIntent.getActivity(
+            context, widgetId,
+            preferencesActivityIntent,
+            PendingIntent.FLAG_CANCEL_CURRENT);
 
-		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-		remoteViews.setOnClickPendingIntent(R.id.container, intent);
-		setWeeksVisibility(remoteViews, widget);
-		setCellsColor(remoteViews, widget, userActivity);
-		widgetManager.updateAppWidget(widgetId, remoteViews);
-	}
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        remoteViews.setOnClickPendingIntent(R.id.container, intent);
+        setWeeksVisibility(remoteViews, widget);
+        setCellsColor(remoteViews, widget, userActivity);
+        widgetManager.updateAppWidget(widgetId, remoteViews);
+    }
 
-	private void fillUserActivityToFullWeek(List<DailyUserActivity> userActivity) {
+    private void fillUserActivityToFullWeek(List<DailyUserActivity> userActivity) {
 
-		Calendar lastDay = Calendar.getInstance();
-		lastDay = DateUtils.truncate(lastDay, Calendar.DAY_OF_MONTH);
-		L.trace("Today: {}", lastDay.getTime().toString());
+        Calendar lastDay = Calendar.getInstance();
+        lastDay = DateUtils.truncate(lastDay, Calendar.DAY_OF_MONTH);
+        L.trace("Today: {}", lastDay.getTime().toString());
 
-		//TODO Get preferred last day of week from user preferences
-		int preferredLastDayOfWeek = Calendar.SUNDAY;
-		while (lastDay.get(Calendar.DAY_OF_WEEK) != preferredLastDayOfWeek) {
-			lastDay.add(Calendar.DAY_OF_WEEK, 1);
-		}
+        //TODO Get preferred last day of week from user preferences
+        int preferredLastDayOfWeek = Calendar.SUNDAY;
+        while (lastDay.get(Calendar.DAY_OF_WEEK) != preferredLastDayOfWeek) {
+            lastDay.add(Calendar.DAY_OF_WEEK, 1);
+        }
 
-		DailyUserActivity lastSavedUserActivity = userActivity.get(userActivity.size() - 1);
+        DailyUserActivity lastSavedUserActivity = userActivity.get(userActivity.size() - 1);
 
-		Calendar currentDay = Calendar.getInstance();
-		currentDay.setTime(lastSavedUserActivity.getDate());
-		currentDay = DateUtils.truncate(currentDay, Calendar.DAY_OF_MONTH);
+        Calendar currentDay = Calendar.getInstance();
+        currentDay.setTime(lastSavedUserActivity.getDate());
+        currentDay = DateUtils.truncate(currentDay, Calendar.DAY_OF_MONTH);
 
-		while (currentDay.before(lastDay)) {
-			L.trace("Add fictive day: {}", currentDay.getTime().toString());
-			userActivity.add(new DailyUserActivity(currentDay.getTime(), 0));
-			currentDay.add(Calendar.DAY_OF_MONTH, 1);
-		}
+        while (currentDay.before(lastDay)) {
+            L.trace("Add fictive day: {}", currentDay.getTime().toString());
+            userActivity.add(new DailyUserActivity(currentDay.getTime(), 0));
+            currentDay.add(Calendar.DAY_OF_MONTH, 1);
+        }
 
-	}
+    }
 
-	private void setCellsColor(RemoteViews remoteViews, ActivityWidget widget, List<DailyUserActivity> userActivity) {
-		List<Integer> cellIds = widget.getCellViewIdsDesc();
+    private void setCellsColor(RemoteViews remoteViews, ActivityWidget widget, List<DailyUserActivity> userActivity) {
+        List<Integer> cellIds = widget.getCellViewIdsDesc();
 
-		ActivityColor minColor = ActivityColor.Companion.fromIntValue(Color.parseColor("#30FFFFFF"));
-		ActivityColor maxColor = ActivityColor.Companion.fromIntValue(Color.parseColor("#FFFFFFFF"));
+        ActivityColor minColor = ActivityColor.Companion.fromIntValue(Color.parseColor("#30FFFFFF"));
+        ActivityColor maxColor = ActivityColor.Companion.fromIntValue(Color.parseColor("#FFFFFFFF"));
 
-		int maxActivity = findMaxActivity(userActivity);
+        int maxActivity = findMaxActivity(userActivity);
 
-		int idIndex = cellIds.size() - 1;
-		int activityIndex = userActivity.size() - 1;
+        int idIndex = cellIds.size() - 1;
+        int activityIndex = userActivity.size() - 1;
 
-		// Walk from last week to earlier until views end or activity end
-		while (idIndex >= 0 && activityIndex >= 0) {
+        // Walk from last week to earlier until views end or activity end
+        while (idIndex >= 0 && activityIndex >= 0) {
 
-			Integer id = cellIds.get(idIndex);
-			DailyUserActivity activity = userActivity.get(activityIndex);
+            Integer id = cellIds.get(idIndex);
+            DailyUserActivity activity = userActivity.get(activityIndex);
 
-			int color = ActivityColor.Companion.calculateColor(minColor, maxColor, maxActivity, activity.getCount());
-			remoteViews.setInt(id, "setBackgroundColor", color);
+            int color = ActivityColor.Companion.calculateColor(minColor, maxColor, maxActivity, activity.getCount());
+            remoteViews.setInt(id, "setBackgroundColor", color);
 
-			idIndex--;
-			activityIndex--;
-		}
-	}
+            idIndex--;
+            activityIndex--;
+        }
+    }
 
-	private void setWeeksVisibility(RemoteViews remoteViews, ActivityWidget widget) {
-		List<Integer> weekViewIds = widget.getWeekViewIds();
+    private void setWeeksVisibility(RemoteViews remoteViews, ActivityWidget widget) {
+        List<Integer> weekViewIds = widget.getWeekViewIds();
 
-		int visibleWeeksCount = widget.getVisibleWeeksCount();
+        int visibleWeeksCount = widget.getVisibleWeeksCount();
 
-		for (int week = 0; week < weekViewIds.size(); week++) {
-			Integer weekId = weekViewIds.get(week);
-			int visibility = week < visibleWeeksCount ? View.VISIBLE : View.GONE;
-			remoteViews.setViewVisibility(weekId, visibility);
-		}
-	}
+        for (int week = 0; week < weekViewIds.size(); week++) {
+            Integer weekId = weekViewIds.get(week);
+            int visibility = week < visibleWeeksCount ? View.VISIBLE : View.GONE;
+            remoteViews.setViewVisibility(weekId, visibility);
+        }
+    }
 
-	public int findMaxActivity(List<DailyUserActivity> userActivity) {
-		int maxActivity = 0;
-		for (DailyUserActivity activity : userActivity) {
-			maxActivity = Math.max(maxActivity, activity.getCount());
-		}
-		return maxActivity;
-	}
+    public int findMaxActivity(List<DailyUserActivity> userActivity) {
+        int maxActivity = 0;
+        for (DailyUserActivity activity : userActivity) {
+            maxActivity = Math.max(maxActivity, activity.getCount());
+        }
+        return maxActivity;
+    }
 
 }
